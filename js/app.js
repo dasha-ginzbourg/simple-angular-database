d744b3fff48d4c43a2d3359c9fc209c9fd25e293
ginzbourg <dasha.ginzbourg@gmail.com>
2017-02-20 14:27:26 -0800

    Simple Dog Database
    A simple example of a search page made with AngularJS.

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var myApp = angular.module('myApp',[
+  'ngRoute',
+  'myAppControllers'
+  ]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/list', {
+      templateUrl: 'partials/list.html',
+      controller: 'ListController'
+    }).
+    when('/details/:itemId', {
+      templateUrl: 'partials/details.html',
+      controller: 'DetailsController'
+    }).
+    when('/details/:itemId', {
+      templateUrl: 'partials/details.html',
+      controller: 'DetailsController'
+    }).
+    otherwise({
+      redirectTo:'/list'
+    });
+}]);
